6b4e7f960dc7a7c5ac905f4641720ac1e868beaa
jotaaltda <jotape.hin@gmail.com>
2026-04-01 19:27:16 -0300

    Adiciona teste de leitura caractere por caractere

--- 2026.1/AEDI/Trabalhos/Trabalho de Aquecimento/aquecimento.c
+++ 2026.1/AEDI/Trabalhos/Trabalho de Aquecimento/aquecimento.c
@@ -3,6 +3,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Variáveis globais e protótipos ===========================================================================
+
+FILE *arquivo;
+int caractere;
+
 // Função main ==============================================================================================
 
-int main(){}
+int main()
+{
+
+    arquivo = fopen("input.dat", "r");
+
+    if (arquivo == NULL)
+    {
+
+        printf("Arquivo não encontrado!");
+        exit(1);
+    }
+
+    do
+    {
+
+        caractere = fgetc(arquivo);
+
+        if (caractere != EOF)
+        {
+
+            printf("%c", caractere);
+        }
+
+    } while (caractere != EOF);
+
+    fclose(arquivo);
+
+    return 0;
+}
