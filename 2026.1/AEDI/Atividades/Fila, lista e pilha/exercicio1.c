9d2941058cbfeda97fbf3d255024b4ae317c7c7b
Joao Paulo <jotape.hin@gmail.com>
2026-04-15 12:10:48 -0300

    Cria laços

---
+++ 2026.1/AEDI/Atividades/Fila, lista e pilha/exercicio1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+
+    srand(time(NULL));
+
+    int n = 50;
+    int vetor[n];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        vetor[i] = rand() % (10 - (-10) + 1) + (-10);
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+
+        int indice = rand() % (n - 0 + 1) + 0;
+        int subvetor[indice];
+
+        printf("Vetor %d: ", j);
+
+        for (int k = 0; k < indice; k++)
+        {
+
+            subvetor[k] = rand() % (10 - (-10) + 1) + (-10);
+
+            printf("%d ", subvetor[k]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
